42df0164058b832d19296cb0ade2f81171f75e21
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-08-14 13:13:24 +0530

    added styled components

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron';
+
+export default function Home() {
+    return (
+        <Jumbotron.Container>
+            <Jumbotron.Title>Hello</Jumbotron.Title>
+            <Jumbotron.SubTitle>Hello</Jumbotron.SubTitle>
+        </Jumbotron.Container>
+    )
+}
