073e2f85fa70430956be36abde787ad352d46b9b
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-08-15 22:47:33 +0530

    added header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart
+                        your membership.</OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
+
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
