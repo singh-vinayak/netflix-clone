446ecafd62846b623885bee4307b9bcec319213c
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-08-20 01:18:52 +0530

    added profile in browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import { Header } from '../components';
+import React, { useState, useEffect, useContext } from 'react';
+import { Loading, Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
@@ -18,8 +18,16 @@ export function BrowseContainer() {
         photoURL: "1"
     };
 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+    }, [user])
+
     return profile.displayName ? (
         <>
+            {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
             <Header src="joker1" dontShowOnSmallViewPort>
                 <Header.Frame>
                     <Header.Group>
@@ -37,6 +45,19 @@ export function BrowseContainer() {
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link>{user.displayName}</Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link onClick={() => firebase.auth().signOut()}>
+                                        Sign out</Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
                     </Header.Group>
                 </Header.Frame>
 
