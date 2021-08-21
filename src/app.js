e55d087e37a5b36cde6262c5509eaa9590c8b852
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-08-21 20:23:29 +0530

    added auth hook

--- src/app.js
+++ src/app.js
@@ -3,23 +3,27 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
+import { useAuthListener } from './hooks';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export function App() {
+    const { user } = useAuthListener();
+
     return (
         <Router>
             <Switch>
-                <Route path={ROUTES.SIGN_IN}>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
                     <Signin />
-                </Route>
-                <Route path={ROUTES.SIGN_UP}>
+                </IsUserRedirect>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
                     <Signup />
-                </Route>
-                <Route path={ROUTES.BROWSE}>
+                </IsUserRedirect>
+                <ProtectedRoute user={user} path={ROUTES.BROWSE}>
                     <Browse />
-                </Route>
-                <Route path={ROUTES.HOME}>
+                </ProtectedRoute>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
                     <Home />
-                </Route>
+                </IsUserRedirect>
             </Switch>
         </Router>
     );
